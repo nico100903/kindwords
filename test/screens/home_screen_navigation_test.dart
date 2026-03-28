da0e8ec391cde4e9ae09a3eba43530c90ad7ff87
Mark Cervantes <cmarkalfonso@yahoo.com>
2026-03-28 23:28:27 +0800

    fix(qa): remove unnecessary dart:async import from navigation test

--- test/screens/home_screen_navigation_test.dart
+++ test/screens/home_screen_navigation_test.dart
@@ -11,8 +11,6 @@
 //
 // DO NOT fix these tests. They are the contract.
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
